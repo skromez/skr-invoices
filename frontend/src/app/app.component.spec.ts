5cb168ab076769aedb954da9910a595e00456b7f
Dmitrii Novikov <kromez12@gmail.com>
2024-04-20 21:57:46 +0200

    fix tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
+    expect(compiled.querySelector('#welcome')?.textContent).toContain(
       'Welcome frontend'
     );
   });
